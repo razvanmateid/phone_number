43a61b35944080bbcf1c3a18b851fb18f5afe8b7
Mircea Badragan <mircea.badragan@iwelcome.com>
2018-10-23 14:09:36 +0300

    material-ui dependency and style related fixes

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-import { storiesOf } from "@storybook/react";
+import {storiesOf} from "@storybook/react";
 
 import '../rrui.css'
 import '../style.css'
 import PhoneInput from '../index';
 
+// material ui related
+import TextField from 'material-ui/TextField';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 storiesOf("Phone Component", module)
     .add("with text", () => (
-        <PhoneInput onChange={_ => console.log('change')}/>
+        <MuiThemeProvider>
+            <PhoneInput
+                onChange={_ => console.log('change')}
+                inputComponent={TextField}
+            />
+        </MuiThemeProvider>
     ));
