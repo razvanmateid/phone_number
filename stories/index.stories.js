d4d2ce9bb94dc11a903055a002b9a70e9f52854c
Mircea Badragan <mircea.badragan@iwelcome.com>
2019-01-28 15:41:15 +0200

    upgrade to latest material-ui

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -7,16 +7,14 @@ import '../style.css'
 import PhoneInput from '../index';
 
 // material ui related
-import TextField from 'material-ui/TextField';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from '@material-ui/core/TextField';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 storiesOf("MUI Phone Component", module)
     .add("Basic", () => (
-        <MuiThemeProvider>
-            <PhoneInput
-                onChange={_ => console.log('change')}
-                inputComponent={TextField}
-                displayInitialValueAsLocalNumber={true}
-            />
-        </MuiThemeProvider>
+        <PhoneInput
+            onChange={_ => console.log('change')}
+            inputComponent={TextField}
+            displayInitialValueAsLocalNumber={true}
+        />
     ));
